13da28ce563f2c046f417fc7e1873073e8791597
Rakshya-Panday <rakshyapanday202@gmail.com>
2024-02-18 14:15:43 +0545

    adding the first file of javascript

---
+++ Javascript/index.js
@@ -0,0 +1,43 @@
+// console.log("hello world")
+let a = 5;
+
+//Javascript is a dynamic language so semicolon is not compulasory
+// let a = 6  which is invalid we cannot redeclare means we can update the value  the let and the scope of variable is block level
+// const b =8  in constant we cannote redeclare and reinitize the  value in constant
+
+// var v = 4;  in var we can redeclare and reinitize the value
+// console.log(a)
+
+//DataTypes in Javascript
+//1. Number = interger,float
+
+let x = 5;
+x= 1.1;
+
+//2. String = "" , ''
+let name = "rakshya";
+
+//3.Boolean = true/false
+
+let b = true;
+
+
+//4. Undefined 
+
+let c ;
+
+//5. Null
+let d = null
+
+//6. Object = array,object,date
+
+let obj = {name :"rakshya",address :"shankhamul"}
+let array = [1,"abc",'rakshya']
+let date = new Date()
+console.log(date)
+
+//7. Symbol
+
+let sum = "a" + 123 +2;
+sum =  123 +2 +"a" 
+// console.log(sum)
