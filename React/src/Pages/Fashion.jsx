6ca0c198b95c44ac1fc196d25f91ff99d92dd426
Rakshya-Panday <rakshyapanday202@gmail.com>
2024-03-19 10:05:04 +0545

    Add the CartSlice

--- React/src/Pages/Fashion.jsx
+++ React/src/Pages/Fashion.jsx
@@ -1,61 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Fashion = () => {
-  const[products,setProducts] = useState([]);
-  const[limit,setLimit] = useState(8)
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then( data => setProducts(data.products.slice(0,limit)))
-    .catch(console.log("there is an effect while load the data"))
-  },[])
-//  const products = [
-//     {},{},{},{},{},{},{},{},{}
-//   ]
+  const [products, setProducts] = useState([]);
+  const [limit, setLimit] = useState(8);
+
+  
+
+ useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+
   return (
     <>
-    <div className='grid grid-cols-4 mx-auto'>
-      < div className=' col-span-1 px-8 min-h-screen '>
-        <h1 className='text-2xl '>Deals</h1>
-        <ul>
-         <Link to= "#"> <li>New Year Offer</li></Link>
-         <Link to= "#"> <li>New Year Offer</li></Link>
-         <Link to= "#"> <li>New Year Offer</li></Link>
-          
-        </ul>
-        <h1>Category</h1>
-        <input type="checkbox" id='men'/><label htmlFor="men">Mens</label>
-        <input type="checkbox" /><label htmlFor="">Mens</label>
-        <input type="checkbox" /><label htmlFor="">Mens</label>
-        <input type="checkbox" /><label htmlFor="">Mens</label>
+      <div className="grid grid-cols-4 mx-auto">
+        <div className=" col-span-1 px-8 min-h-screen ">
+          <h1 className="text-2xl ">Deals</h1>
+          <ul>
+            <Link to="#">
+              {" "}
+              <li>New Year Offer</li>
+            </Link>
+            <Link to="#">
+              {" "}
+              <li>New Year Offer</li>
+            </Link>
+            <Link to="#">
+              {" "}
+              <li>New Year Offer</li>
+            </Link>
+          </ul>
+          <h1>Category</h1>
+          <input type="checkbox" id="men" />
+          <label htmlFor="men">Mens</label>
+          <input type="checkbox" />
+          <label htmlFor="">Mens</label>
+          <input type="checkbox" />
+          <label htmlFor="">Mens</label>
+          <input type="checkbox" />
+          <label htmlFor="">Mens</label>
 
-        <h1>Price Range</h1>
-        <input type="radio" /><label htmlFor="">upto 1000</label>
-        <input type="radio" /><label htmlFor="">upto 5000</label>
-        <input type="radio" /><label htmlFor="">upto 10000</label>
-        <input type="radio" /><label htmlFor="">upto 100000</label>
+          <h1>Price Range</h1>
+          <input type="radio" />
+          <label htmlFor="">upto 1000</label>
+          <input type="radio" />
+          <label htmlFor="">upto 5000</label>
+          <input type="radio" />
+          <label htmlFor="">upto 10000</label>
+          <input type="radio" />
+          <label htmlFor="">upto 100000</label>
+        </div>
+        <div className=" col-span-3  grid grid-cols-4 ">
+          {products.map((product) => (
+            <div className="card p-5 col-span-1 border-2 m-2">
+              <img
+                src={product.image}
+                alt="image"
+                style={{ width: "100%", height: "250px" }}
+              />
+              <h2>{product.title}</h2>
+              <h3>${product.price}</h3>
+              <button className="bg-blue-500  text-white w-100 rounded p-2">
+                Add to Cart
+              </button>
+            </div>
+          ))}
+          <button onClick={() => setLimit(limit + 4)} className="bg-blue-500">
+            Show More
+          </button>
+        </div>
       </div>
-      <div className=' col-span-3  grid grid-cols-4'>
-        {
-          products.map((product)=>(
-            <div className='card p-5 col-span-1'>
-            <img src={product.images}alt=""  style={{width :"100%",height:"250px"}}/>
-            <h2>Title</h2>
-            <h3>Price</h3>
-            <button className='bg-green-500 text-white w-100 rounded p-3'>Add to Cart</button>
-             
-          </div>
-          ))
-        }
-        <button onClick={()=>(setLimit(limit+4))} className= "bg-blue-500">Show More</button>
-       
-      </div>
-
-    </div>
-    
     </>
-  )
-}
+  );
+};
 
-export default Fashion
+export default Fashion;
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Home from './Pages/Home.jsx';
-import About from './Pages/About.jsx';
-import Contact from './Pages/Contact.jsx';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Login from './Pages/Login.jsx';
-import Register from './Pages/Register.jsx';
-import Services from './Pages/Services.jsx';
-import Cart from './Pages/Cart.jsx';
-import Fashion from './Pages/Fashion.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
+import Services from "./Pages/Services.jsx";
+import Cart from "./Pages/Cart.jsx";
+import Fashion from "./Pages/Fashion.jsx";
+import { Provider } from "react-redux";
+import {store} from './Redux/Store.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element = {<App/>}>
-      <Route path='/'element = {<Home/>}/>
-      <Route path='/about'element = {<About/>}/>
-      <Route path='/contact'element = {<Contact/>}/>
-      <Route path='/login'element = {<Login/>}/>
-      <Route path='/register'element = {<Register/>}/>
-      <Route path='/services'element = {<Services/>}/>
-      <Route path='/cart'element = {<Cart/>}/>
-      <Route path='/fashion'element = {<Fashion/>}/>
-    
-
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/fashion" element={<Fashion />} />
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <Provider store = {store}>
+    <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
